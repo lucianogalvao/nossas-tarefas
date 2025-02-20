8717376e80cf91d112d3865ed72c2fcb7baf0b0e
Luciano <luciano.g.galvaao@gmail.com>
2025-02-20 10:31:05 -0300

    feat: backend configuracoes iniciais de rota, mongo e cors

---
+++ nossas-tarefas-backend/src/config/db.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const mongoURI = process.env.MONGODB_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("MongoDB error:", err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ nossas-tarefas-backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db.js");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT}`);
+});
+
+module.exports = app;
