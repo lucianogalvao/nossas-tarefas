011501c3800fd36b598dcdef7186784fae6755ca
Luciano <luciano.g.galvaao@gmail.com>
2025-02-21 14:21:33 -0300

    feat: frontend - variaveis de ambiente no next

--- nossas-tarefas-app/next.config.ts
+++ nossas-tarefas-app/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+
+  env: {
+    FRONTEND_URL: process.env.FRONTEND_URL,
+    BACKEND_URL: process.env.BACKEND_URL,
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_API_ID: process.env.FIREBASE_API_ID,
+    FIREBASE_API_VAPID_KEY: process.env.FIREBASE_API_VAPID,
+  },
 };
 
 export default nextConfig;
