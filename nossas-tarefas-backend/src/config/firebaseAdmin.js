54d3ad66ff0fc06adfc13fa71c5d37c4d89bb1b6
Luciano <luciano.g.galvaao@gmail.com>
2025-02-21 14:32:43 -0300

    fix: anula firebase (to-do)

--- nossas-tarefas-backend/src/config/firebaseAdmin.js
+++ nossas-tarefas-backend/src/config/firebaseAdmin.js
@@ -1,16 +1,16 @@
-// config/firebaseAdmin.js
-const admin = require('firebase-admin');
+// // config/firebaseAdmin.js
+// const admin = require('firebase-admin');
 
-const serviceAccountPath = process.env.FIREBASE_PRIVATE_KEY_PATH;
+// const serviceAccountPath = process.env.FIREBASE_API_PRIVATE_KEY;
 
-if (!serviceAccountPath) {
-  throw new Error("Caminho para a chave privada não está definido no .env");
-}
+// if (!serviceAccountPath) {
+//   throw new Error("Caminho para a chave privada não está definido no .env");
+// }
 
-admin.initializeApp({
-  credential: admin.credential.cert(require(serviceAccountPath)),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(require(serviceAccountPath)),
+// });
 
-const messaging = admin.messaging();
+// const messaging = admin.messaging();
 
-module.exports = messaging;
+// module.exports = messaging;
